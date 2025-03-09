925e2a2c34f2e114fab4d61d4112bf7c46b255a7
0xgb1 <101956347+0xgb1@users.noreply.github.com>
2025-03-09 18:25:05 -0400

    minimum working example of reading csv + StringRecord output

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use csv::ReaderBuilder;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let input_path = match std::env::args_os().nth(1) {
+        Some(path) => path,
+        None => {
+            eprintln!("Usage error: no path specified. Please specify an input path.");
+            std::process::exit(1);
+        }
+    };
+
+    let rdr = ReaderBuilder::new().flexible(true).from_path(input_path)?;
+    for result in rdr.into_records() {
+        let line = result?;
+        println!("{:?}", line);
+    }
+    Ok(())
 }
